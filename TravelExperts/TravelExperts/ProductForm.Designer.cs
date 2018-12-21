098413dd4376884bea7ed645b5dce86c7c381efa
James Cockriell <632947@ACDM.DS.SAIT.CA>
2018-12-21 09:34:28 -0700

    created products querry form
    still need to create the querrey to display data

--- TravelExperts/TravelExperts/ProductForm.Designer.cs
+++ TravelExperts/TravelExperts/ProductForm.Designer.cs
@@ -28,21 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductForm));
+            this.dgvPackageList = new System.Windows.Forms.DataGridView();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.addlStripButton = new System.Windows.Forms.ToolStripButton();
+            this.editStripButton = new System.Windows.Forms.ToolStripButton();
+            this.deleteStripButton = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPackageList)).BeginInit();
+            this.toolStrip2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dgvPackageList
+            // 
+            this.dgvPackageList.AllowUserToAddRows = false;
+            this.dgvPackageList.AllowUserToDeleteRows = false;
+            this.dgvPackageList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvPackageList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPackageList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPackageList.Location = new System.Drawing.Point(8, 37);
+            this.dgvPackageList.MultiSelect = false;
+            this.dgvPackageList.Name = "dgvPackageList";
+            this.dgvPackageList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPackageList.Size = new System.Drawing.Size(964, 418);
+            this.dgvPackageList.TabIndex = 4;
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.AutoSize = false;
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addlStripButton,
+            this.editStripButton,
+            this.deleteStripButton});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(984, 35);
+            this.toolStrip2.TabIndex = 3;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // addlStripButton
+            // 
+            this.addlStripButton.AutoSize = false;
+            this.addlStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.addlStripButton.Image = ((System.Drawing.Image)(resources.GetObject("addlStripButton.Image")));
+            this.addlStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.addlStripButton.Name = "addlStripButton";
+            this.addlStripButton.Padding = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.addlStripButton.Size = new System.Drawing.Size(32, 32);
+            this.addlStripButton.Text = "ADD";
+            this.addlStripButton.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            // 
+            // editStripButton
+            // 
+            this.editStripButton.AutoSize = false;
+            this.editStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.editStripButton.Image = ((System.Drawing.Image)(resources.GetObject("editStripButton.Image")));
+            this.editStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.editStripButton.Name = "editStripButton";
+            this.editStripButton.Size = new System.Drawing.Size(32, 32);
+            this.editStripButton.Text = "EDIT  F2";
+            // 
+            // deleteStripButton
+            // 
+            this.deleteStripButton.AutoSize = false;
+            this.deleteStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.deleteStripButton.Image = ((System.Drawing.Image)(resources.GetObject("deleteStripButton.Image")));
+            this.deleteStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.deleteStripButton.Name = "deleteStripButton";
+            this.deleteStripButton.Size = new System.Drawing.Size(32, 32);
+            this.deleteStripButton.Text = "DELETE";
             // 
             // ProductForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.dgvPackageList);
+            this.Controls.Add(this.toolStrip2);
             this.Location = new System.Drawing.Point(200, 0);
             this.Name = "ProductForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "ProductForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPackageList)).EndInit();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvPackageList;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton addlStripButton;
+        private System.Windows.Forms.ToolStripButton editStripButton;
+        private System.Windows.Forms.ToolStripButton deleteStripButton;
     }
 }
